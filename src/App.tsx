b1c220958dd4e7a1eb9208e6a0a70c6d5a117a52
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-06-04 03:07:02 -0400

    feat: enhance App component with URL fetching and redirection logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import { useCreateShortUrl } from './hooks';
+import { useEffect } from 'react';
+import { useCreateShortUrl, useGetUrlByLabel, useReadUrls } from './hooks';
 import { handleFormData } from './utils';
+import { useUrls } from './context';
+
 /* 
 ! Basic URL structure
 ! protocol://subDomain.domain
@@ -7,24 +10,41 @@ import { handleFormData } from './utils';
 
 export default function App() {
     const { createShortUrl } = useCreateShortUrl();
+    const { readUrls } = useReadUrls();
+    const { setUrls } = useUrls();
+    const { getUrlByLabel } = useGetUrlByLabel();
+
+    useEffect(() => {
+        (async () => {
+            const responseUrls = await readUrls();
+
+            if (responseUrls) {
+                setUrls(responseUrls);
+            }
+
+            const label = window.location.pathname.slice(1);
+
+            const url = await getUrlByLabel(label);
+            if (url?.originalUrl) window.location.href = url?.originalUrl;
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const $INPUTS = e.target.querySelectorAll('input');
+        const form = e.currentTarget;
+        const $INPUTS = form.querySelectorAll('input');
         const formateData = handleFormData($INPUTS);
 
-        console.log(formateData);
         createShortUrl(formateData);
     };
-
-    const inputDefault: string = 'border';
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="" id="originalUrl" className={`${inputDefault}`} />
-                <input type="text" name="" id="label" className={`${inputDefault}`} />
+                <input type="text" name="" id="originalUrl" className="border" />
+                <input type="text" name="" id="label" className="border" />
                 <button type="submit">submit</button>
             </form>
         </div>
--- src/hooks/useCreateUrl.ts
+++ src/hooks/useCreateUrl.ts
@@ -1,10 +1,16 @@
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../services';
 import type { CreateShortUrlT } from '../types';
+import { useGetUrlByLabel } from '../hooks';
 
 export function useCreateShortUrl() {
+    const { getUrlByLabel } = useGetUrlByLabel();
+
     async function createShortUrl({ label, originalUrl }: CreateShortUrlT) {
         try {
+            const existingUrl = await getUrlByLabel(label);
+            if (existingUrl) throw new Error('URL with this label already exists');
+
             await addDoc(collection(db, 'urls'), {
                 label: `/${label}`,
                 originalUrl,
