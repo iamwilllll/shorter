a0885d64dc7c8482896eeceaf238910e2821fca0
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-06-08 13:19:27 -0400

    feat: update LandingPage to include LandingHero component and enhance layout structure

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,12 @@
-import { LandingNav } from '@/components';
+import { LandingNav, LandingHero } from '@/components';
 
 export function LandingPage() {
     return (
-        <section className="flex size-full flex-col p-5">
-            <LandingNav />
+        <section className="box-border grid h-screen max-h-screen grid-cols-1 grid-rows-[auto_1fr] place-items-center gap-5 p-5">
+            <LandingNav className="col-start-1 row-start-1 w-full" />
+            <div className="col-start-1 row-start-2 h-full min-h-0 w-full">
+                <LandingHero className="h-full w-full" />
+            </div>
         </section>
     );
 }
