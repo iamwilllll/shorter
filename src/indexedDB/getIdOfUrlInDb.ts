ff994bec5b7e1684dd3dd191eb80bac9f5673d30
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-06-18 03:19:44 -0400

    feat: add getIdOfUrlsInDB function to retrieve URL IDs from IndexedDB

---
+++ src/indexedDB/getIdOfUrlInDb.ts
@@ -0,0 +1,22 @@
+import { getStore } from '@/indexedDB';
+import { type ShortUrlT } from '@/types';
+
+export const getIdOfUrlsInDB = async (): Promise<string[]> => {
+    const store = await getStore();
+
+    return new Promise((resolve, reject) => {
+        const request = store.getAll();
+
+        request.onsuccess = () => {
+            const urls = request.result as ShortUrlT[];
+
+            const urlIds = urls.filter((url) => typeof url.id === 'string').map((url) => url.id);
+
+            resolve(urlIds);
+        };
+
+        request.onerror = () => {
+            reject(request.error);
+        };
+    });
+};
