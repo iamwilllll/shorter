461a5764e512f08ab1e85fcdc6f9738f1d4de0aa
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-06-29 03:43:21 -0400

    fix: update layout exports to include Dashboard components and reorder page exports

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,2 +1,6 @@
 export * from './common/Footer';
 export * from './common/AppLayout';
+
+export * from './dashboard/DashboardNav';
+export * from './dashboard/Dashboard';
+export * from './dashboard/DashboardHeader';
